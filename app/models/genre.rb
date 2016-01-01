8cba927bcf4bc0cac34ae7203e60745304252a48
Mitul Mistry <mitul.mistry1@gmail.com>
2016-01-01 18:20:19 +0000

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,13 +4,16 @@ class Genre < ActiveRecord::Base
 
   def song_count
     # return the number of songs in a genre
+    songs.size
   end
 
   def artist_count
     # return the number of artists associated with the genre
+    artists.size
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    artists.collect{ |i| i.name }
   end
 end
