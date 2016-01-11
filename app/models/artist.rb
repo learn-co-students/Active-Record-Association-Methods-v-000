f123671f27eec3e4ce142ad1febc33d4cc2052d8
lisamarie616 <lisamarie616@gmail.com>
2016-01-10 17:47:42 -0800

    refactor methods in artist and genre classes

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -14,6 +14,7 @@ class Artist < ActiveRecord::Base
 
   def genre_count
     #return the number of genres associated with the artist
-    self.songs.collect {|song| song.genre}.uniq.count
+    # self.songs.collect {|song| song.genre}.uniq.count
+    self.genres.count
   end
 end
