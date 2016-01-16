cde2f4fcf2a30b69c2e7cb667f179567058f4b78
eric-an <erican@me.com>
2016-01-16 02:49:04 -0800

    define methods to write queries using ActiveRecord macros

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,15 +2,23 @@ class Genre < ActiveRecord::Base
   has_many :songs
   has_many :artists, through: :songs
 
+  @@artists_list = []
+  
   def song_count
     # return the number of songs in a genre
+    self.songs.count
   end
 
   def artist_count
     # return the number of artists associated with the genre
+    self.songs.each { |song| @@artists_list << song.artist }
+    @@artists_list.uniq.count
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    artists_names = []
+    @@artists_list.each { |artist| artists_names << artist.name }
+    artists_names
   end
 end
