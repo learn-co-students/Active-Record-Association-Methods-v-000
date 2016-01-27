19fcb217a04105ed3858566aeca54aeddb94151a
Chen Yang <yangc5@mail.uc.edu>
2016-01-27 20:02:10 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,11 +1,16 @@
+require_relative './artist.rb'
+
 class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
 
   def get_genre_name
+    self.genre.name
   end
 
   def drake_made_this
-    # when this method is called it should assign the song's artist to Drake
+    drake = Artist.new(name: 'Drake')
+    self.artist=drake
   end
+
 end
