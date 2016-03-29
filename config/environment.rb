8d913e9923846b1e432ceaec98f2385c7bfa2d41
Valentin Rosu <alispace410@gmail.com>
2016-03-29 07:21:35 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -15,11 +15,12 @@ if ENV["PLAYLISTER_ENV"] == "test"
 end
 
 def migrate_db
+  puts __FILE__
   DB.tables.each do |table|
     DB.execute("DROP TABLE #{table}")
   end
 
-  Dir[File.join(File.dirname(__FILE__), "../db/migrate", "*.rb")].each do |f| 
+  Dir[File.join(File.dirname(__FILE__), "../db/migrate", "*.rb")].sort!.each do |f|
     require f
     migration = Kernel.const_get(f.split("/").last.split(".rb").first.gsub(/\d+/, "").split("_").collect{|w| w.strip.capitalize}.join())
     migration.migrate(:up)
