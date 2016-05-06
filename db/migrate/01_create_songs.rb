1106ad515b684db7e5b1eff441743f70ef89fce5
Hiral Desai <hiral.desai83@gmail.com>
2016-05-06 01:19:01 -0400

    Done.

--- db/migrate/01_create_songs.rb
+++ db/migrate/01_create_songs.rb
@@ -1,7 +1,9 @@
-class CreateSongs < ActiveRecord::Migration
+class CreateSongs <ActiveRecord::Migration
   def change
     create_table :songs do |t|
-      t.string :name 
+      t.string :name
+      t.integer :artist_id
+      t.integer :genre_id
     end
   end
 end
