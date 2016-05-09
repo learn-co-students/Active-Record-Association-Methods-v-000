0a3f1d8dcadb078ba417a87a90955d4267d4462e
CodeByLine <yleventhal@ymail.com>
2016-05-09 15:28:18 -0400

    worked locally; have to figure out how to upload. Spent hours trying to sort out gems

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,17 +3,34 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
-    #return the genre of the artist's first saved song
-    self.songs.genre.first
+    self.songs.first.genre
   end
 
   def song_count
-    #return the number of songs associated with the artist
-    songs.count
+    self.songs.count
   end
 
   def genre_count
-    #return the number of genres associated with the artist
-    genres.count
+    self.genres.count
   end
 end
+
+# class Artist < ActiveRecord::Base
+#   has_many :songs
+#   has_many :genres, through: :songs
+# 
+#   def get_genre_of_first_song
+#     #return the genre of the artist's first saved song
+#     self.songs.genre.first
+#   end
+# 
+#   def song_count
+#     #return the number of songs associated with the artist
+#     self.songs.count
+#   end
+# 
+#   def genre_count
+#     #return the number of genres associated with the artist
+#     genres.count
+#   end
+# end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,11 +1,26 @@
+
 class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
 
   def get_genre_name
+    self.genre.name
   end
 
   def drake_made_this
-    # when this method is called it should assign the song's artist to Drake
+    self.artist = Artist.create(name: "Drake")
   end
 end
+
+
+# class Song < ActiveRecord::Base
+#   belongs_to :artist
+#   belongs_to :genre
+# 
+#   def get_genre_name
+#   end
+# 
+#   def drake_made_this
+#     # when this method is called it should assign the song's artist to Drake
+#   end
+# end
