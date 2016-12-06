2467a64ce88f934639a9c6c0cb685fd8873974c2
mjdele <mike.delehant@gmail.com>
2016-12-05 20:47:46 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,7 +7,7 @@ class Song < ActiveRecord::Base
   end
 
   def drake_made_this
-    drake = Artist.create(name: "Drake")
+    drake = Artist.find_or_create_by(name: "Drake")
     self.artist = drake
   end
 end
