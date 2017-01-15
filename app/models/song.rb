51819d3af30d3e22546f758a68faf4f4b38756ed
Nicholas Ratti <nickratti@gmail.com>
2017-01-15 22:22:17 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,9 +3,13 @@ class Song < ActiveRecord::Base
   belongs_to :genre
 
   def get_genre_name
+    self.genre.name
+    #this isnt pluralized because the song has only one genre
   end
 
   def drake_made_this
     # when this method is called it should assign the song's artist to Drake
+    drake = Artist.find_or_create_by(name: "Drake")
+    self.artist = drake
   end
 end
