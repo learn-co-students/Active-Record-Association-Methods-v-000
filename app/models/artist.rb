ffa97a02a67c80dfe3d0d919144a11cc6676b8c5
Sara Hopper <sara.hopper@gmail.com>
2017-05-08 21:41:57 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,14 +3,14 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
-    #return the genre of the artist's first saved song
+    Genre.first
   end
 
   def song_count
-    #return the number of songs associated with the artist
+    Song.all.count
   end
 
   def genre_count
-    #return the number of genres associated with the artist
+    Genre.all.count
   end
 end
