ea4bacfc7ff7ae6f0263edf36143b9db8ac22d61
Lia HUANG <lisa.yc.huang@gmail.com>
2017-06-20 17:02:00 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,16 +1,27 @@
+require "pry"
+
 class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
+    self.songs.first.genre
   end
 
   def song_count
     #return the number of songs associated with the artist
+    self.songs.size
   end
 
   def genre_count
     #return the number of genres associated with the artist
+    genres = []
+    self.songs.each {|song|
+      if !genres.include?(song.genre.name)
+        genres << song.genre.name
+      end
+    }
+    genres.size
   end
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,13 +4,28 @@ class Genre < ActiveRecord::Base
 
   def song_count
     # return the number of songs in a genre
+    self.songs.count
   end
 
   def artist_count
     # return the number of artists associated with the genre
+    artists = []
+    self.songs.each {|song|
+      if !artists.include?(song.artist.name)
+        artists << song.artist.name
+      end
+    }
+    artists.size
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    artists = []
+    self.songs.each {|song|
+      if !artists.include?(song.artist.name)
+        artists << song.artist.name
+      end
+    }
+    artists
   end
 end
