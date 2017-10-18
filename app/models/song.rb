b51cdffe3c06f5d8848462f8b4b791d455ee26af
GusVilla303 <gusvilla303@gmail.com>
2017-10-17 22:35:52 -0600

    Change #drake_made_this to use find_or_create_by instead of create. All specs are green.

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,8 +7,7 @@ class Song < ActiveRecord::Base
   end
 
   def drake_made_this
-    # when this method is called it should assign the song's artist to Drake
-    artist = Artist.create(name: "Drake")
-    self.artist = artist
+    drake = Artist.find_or_create_by(name: "Drake")
+    self.artist = drake
   end
 end
