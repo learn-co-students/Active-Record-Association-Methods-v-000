7bfadb51ec0383eff33502d670d687d378f7728b
Robert Laws <roblaws@me.com>
2017-10-22 11:33:42 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,13 +4,16 @@ class Artist < ActiveRecord::Base
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
+    Artist.find(1).genres.first
   end
 
   def song_count
     #return the number of songs associated with the artist
+    Artist.find(1).songs.size
   end
 
   def genre_count
     #return the number of genres associated with the artist
+    Artist.find(1).genres.size
   end
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,13 +4,20 @@ class Genre < ActiveRecord::Base
 
   def song_count
     # return the number of songs in a genre
+    Genre.first.songs.size
   end
 
   def artist_count
     # return the number of artists associated with the genre
+    Genre.first.artists.size
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    Genre.all.collect do |g|
+      g.artists.collect do |a|
+        a.name
+      end
+    end.flatten
   end
 end
