cabc3dc1175f72d9831f1ffec02589299a3d30ba
Nour Kodeih <nour.kodeih@gmail.com>
2017-12-18 19:51:28 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,6 +7,6 @@ class Song < ActiveRecord::Base
   end
 
   def drake_made_this
-    self.artist = Artist.create(name: "Drake")
+    self.artist = Artist.find_by(name: "Drake") || Artist.create(name: "Drake")
   end
 end
