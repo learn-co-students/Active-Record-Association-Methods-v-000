f1d335743502290f22d0d9b80d25706408b4aadf
Sally Zasloff <szasloff@gmail.com>
2018-01-16 21:39:23 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,14 +3,14 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
-    self.genres.first
+    self.songs.first.genre
   end
 
   def song_count
-    self.songs.count
+    self.songs.size
   end
 
   def genre_count
-    self.genres.count
+    self.genres.size
   end
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,12 +3,11 @@ class Genre < ActiveRecord::Base
   has_many :artists, through: :songs
 
   def song_count
-    self.songs.count
-    # return the number of songs in a genre
+    self.songs.size
   end
 
   def artist_count
-    self.artists.count
+    self.artists.size
   end
 
   def all_artist_names
