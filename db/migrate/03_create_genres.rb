07e3bfd713866f486805ad2eb23d72ca618170bb
churchwerks <steven.lennis@icloud.com>
2018-01-16 14:21:25 -0600

    Migrate Issues Fixed
    Added [5.1.4] to ActiveRecord::Migration[5.1.4]

--- db/migrate/03_create_genres.rb
+++ db/migrate/03_create_genres.rb
@@ -1,7 +1,7 @@
-class CreateGenres < ActiveRecord::Migration
+class CreateGenres < ActiveRecord::Migration[5.1.4]
   def change
     create_table :genres do |t|
-      t.string :name 
+      t.string :name
     end
   end
 end
