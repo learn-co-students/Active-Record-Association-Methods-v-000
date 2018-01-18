13668ba2932215903ad7d9125acc56eafe46f5a1
churchwerks <steven.lennis@icloud.com>
2018-01-18 09:41:01 -0600

    Final
    Edited song.rb

--- app/models/song.rb
+++ app/models/song.rb
@@ -8,7 +8,6 @@ class Song < ActiveRecord::Base
 
   def drake_made_this
     # when this method is called it should assign the song's artist to Drake
-    #binding.pry
     drake = Artist.find_or_create_by(name: "Drake")
     self.artist = drake
   end
