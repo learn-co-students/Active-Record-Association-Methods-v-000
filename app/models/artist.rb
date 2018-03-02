e4229dad05a31522bc696e7e997a2ab50cbb1973
johnwojo <jwojo13@gmail.com>
2018-03-02 17:21:17 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -9,11 +9,11 @@ class Artist < ActiveRecord::Base
 
   def song_count
     #return the number of songs associated with the artist
-    self.songs.count
+    self.songs.size
   end
 
   def genre_count
     #return the number of genres associated with the artist
-    self.genres.count
+    self.genres.size
   end
 end
