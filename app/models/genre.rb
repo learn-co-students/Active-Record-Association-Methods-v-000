3d9d90dbff6b575b9a7815affe149e4aa3a16f6d
pi-track <patrick.eells@gmail.com>
2018-03-23 10:21:48 -0400

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,14 +3,14 @@ class Genre < ActiveRecord::Base
   has_many :artists, through: :songs
 
   def song_count
-    # return the number of songs in a genre
+    self.songs.size
   end
 
   def artist_count
-    # return the number of artists associated with the genre
+    self.all_artist_names.uniq.size
   end
 
   def all_artist_names
-    # return an array of strings containing every musician's name
+    self.songs.collect {|s| s.artist.name}
   end
 end
