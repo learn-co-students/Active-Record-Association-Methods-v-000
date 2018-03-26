5c2bc5678a41abb9c2bf7cc7a58939410f2542d2
AndrewHongUW <32497926+AndrewHongUW@users.noreply.github.com>
2018-03-26 11:32:52 -0700

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -8,8 +8,11 @@ class Song < ActiveRecord::Base
 
   def drake_made_this
     # when this method is called it should assign the song's artist to Drake
-    drake = Artist.new
-    drake.name = 'Drake'
+    # drake = Artist.new
+    # drake.name = 'Drake'
+    # self.artist = drake
+    # better way and utilizes query methods given by Active Record
+    drake = Artist.find_or_create_by(name: "Drake")
     self.artist = drake
   end
 end
