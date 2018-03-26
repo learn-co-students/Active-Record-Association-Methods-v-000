cfd1bffad387a27af16f5dcdf17fb5b089da21f8
Jay Bednar <ironviralmedia@gmail.com>
2018-03-26 23:55:00 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -16,6 +16,6 @@ class Artist < ActiveRecord::Base
 
   def genre_count
     # return the number of genres associated with the artist
-    self.songs.collect{|s| s.genre}.uniq.count 
+    self.genres.count 
   end
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,6 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.songs.collect{|s| s.artist.name}.uniq 
+    self.artists.count 
   end
 end
