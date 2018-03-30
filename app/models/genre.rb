34c7948781b3d9404b909d3bb3b0e2e108a779a0
Sam. O <samrey2018@gmail.com>
2018-03-30 16:53:34 +0000

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,13 +4,35 @@ class Genre < ActiveRecord::Base
 
   def song_count
     # return the number of songs in a genre
+    self.songs.count
   end
 
   def artist_count
     # return the number of artists associated with the genre
+    self.artists.count
   end
 
   def all_artist_names
-    # return an array of strings containing every musician's name
-  end
+    # when  you see a situation of
+# creating empty array  _ collecting  that array - passing_in and calling the array
+# recipe for .map or .collect
+
+
+contain = self.artists
+contain.map do |instance_of_artist|
+  instance_of_artist.name
+end
+
+  #   # return an array of strings containing every musician's name
+  #   array_of_names = []
+  #   contain = self.artists
+  #   contain.each do |instance_of_artist|
+  #       puts instance_of_artist
+  #       array_of_names << instance_of_artist.name
+  #
+  #
+  #   end
+  #   array_of_names #a.k.a name
+  # end
+
 end
