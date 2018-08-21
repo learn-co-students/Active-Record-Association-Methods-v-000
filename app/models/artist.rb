908b4b23943c6af941ef213e33c35bbcb52e3a7e
Madeline Stark <madeline.stark1@gmail.com>
2018-08-21 17:02:52 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,13 +4,16 @@ class Artist < ActiveRecord::Base
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
+    self.songs.first.genre
   end
 
   def song_count
+    self.songs.count
     #return the number of songs associated with the artist
   end
 
   def genre_count
+    self.genres.count
     #return the number of genres associated with the artist
   end
 end
