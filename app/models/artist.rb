5d2d38d59eaa26216632cba15a336466d961ff08
Barbara Choy <barbchoy@gmail.com>
2018-10-16 16:28:18 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -8,10 +8,10 @@ class Artist < ActiveRecord::Base
   end
 
   def song_count
-    #return the number of songs associated with the artist
+    self.songs.size
   end
 
   def genre_count
-    #return the number of genres associated with the artist
+    self.genres.count
   end
 end
