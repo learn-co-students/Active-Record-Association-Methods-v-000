cd48a59cb979d88de1b39ccc7454149ed4f80577
George  Pianka <gpianka1@gmail.com>
2018-10-16 05:30:07 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,8 @@ Bundler.require
 require 'active_record'
 require 'rake'
 
-Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
-Dir[File.join(File.dirname(__FILE__), "../lib/support", "*.rb")].each {|f| require f}
+Dir[File.join(File.dirname(__FILE__), "../app/**/*.rb")].each {|f| require f}
+Dir[File.join(File.dirname(__FILE__), "../lib/**/*.rb")].each {|f| require f}
 
 DBRegistry[ENV["PLAYLISTER_ENV"]].connect!
 DB = ActiveRecord::Base.connection
