0c7891299d6cf1bc082072a6f9027a7d9b7ed480
Jeremy <jeremy.emata@gmail.com>
2018-11-09 17:11:32 -0500

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,13 +4,18 @@ class Genre < ActiveRecord::Base
 
   def song_count
     # return the number of songs in a genre
+    self.songs.count
   end
 
   def artist_count
     # return the number of artists associated with the genre
+    self.songs.all.where(genre_id: self.id).count
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    self.songs.map do |song|
+       song.artist.name 
+    end
   end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,10 +2,14 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
 
+
   def get_genre_name
+
+     self.genre.name 
   end
 
   def drake_made_this
+    self.artist = Artist.new(name: "Drake")
     # when this method is called it should assign the song's artist to Drake
   end
 end
