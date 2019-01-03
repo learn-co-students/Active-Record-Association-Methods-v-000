cd323eff21a618e1c0ca6ecacd9c22e9cb42f3d0
Steven Crouse <sdcrouse91@gmail.com>
2019-01-03 06:21:48 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -14,7 +14,6 @@ class Artist < ActiveRecord::Base
 
   def genre_count
     #return the number of genres associated with the artist
-    binding.pry
-    #self.genres.count
+    self.genres.count
   end
 end
