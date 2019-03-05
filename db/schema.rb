9c2be6f333c9ee2d957cd352585fffa1363f87bd
taisaoko <ttd3calvin@gmail.com>
2019-03-05 20:17:33 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 5) do
-
-  create_table "artists", force: :cascade do |t|
-    t.string "name"
-  end
-
-  create_table "genres", force: :cascade do |t|
-    t.string "name"
-  end
-
-  create_table "songs", force: :cascade do |t|
-    t.string  "name"
-    t.integer "artist_id"
-    t.integer "genre_id"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
