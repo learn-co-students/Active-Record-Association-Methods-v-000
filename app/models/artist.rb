ff3174c4204f2f2fa08a95a408828821f3e0d3be
Reno Sorensen <renojames19@gmail.com>
2019-04-26 04:43:49 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -7,10 +7,10 @@ class Artist < ActiveRecord::Base
   end
 
   def song_count
-    #return the number of songs associated with the artist
+    self.songs.count
   end
 
   def genre_count
-    #return the number of genres associated with the artist
+    self.genres.uniq.count
   end
 end
