9951e18559ca657c19d30b80e39d38720acdc690
Eric Johnson <ericjohnson0216@icloud.com>
2019-10-20 01:40:12 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,14 +3,14 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
-    puts "hi"
+    self.songs.first.genre
   end
 
   def song_count
-    #return the number of songs associated with the artist
+    self.songs.count
   end
 
   def genre_count
-    #return the number of genres associated with the artist
+    self.genres.count
   end
 end
