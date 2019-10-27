81f8dfbf2d164e2e37eb9bf7768112d0d7787bdb
J Fern <jfern20@gmail.com>
2019-10-27 05:08:33 +0000

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -13,7 +13,7 @@ class Genre < ActiveRecord::Base
   end
 
   def all_artist_names
-    self.artists
+    self.artists.map(:name)
     # return an array of strings containing every musician's name
   end
 end
