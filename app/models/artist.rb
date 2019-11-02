744a18f77c7a082cfd48f273c4055f79126c8b05
github username <github email address>
2019-11-01 20:34:37 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,13 +4,22 @@ class Artist < ActiveRecord::Base
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
+    self.genres.first
   end
 
   def song_count
     #return the number of songs associated with the artist
+    self.songs.length
   end
 
   def genre_count
     #return the number of genres associated with the artist
+    self.genres.length
   end
+
+=begin
+  def get_first_song
+    self.songs.first
+  end
+=end
 end
