575dd70c54a4bb84c3ecf0c3393615ad098635a2
Daniel Suarez <suarez.daniel101@gmail.com>
2021-04-18 23:47:08 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -9,9 +9,13 @@ class Artist < ActiveRecord::Base
 
   def song_count
     #return the number of songs associated with the artist
+    self.songs.count
   end
 
   def genre_count
     #return the number of genres associated with the artist
+    self.songs
+  
+    binding.pry
   end
 end
