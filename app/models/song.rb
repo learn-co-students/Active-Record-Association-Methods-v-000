0eb6ac3b5c441eaba8b687f96b016d18bfd76c81
Daniel Suarez <suarez.daniel101@gmail.com>
2021-04-21 00:18:09 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -10,13 +10,19 @@ class Song < ActiveRecord::Base
     # when this method is called it should assign the song's artist to Drake
     # Drake doesn't exist in the database as an artist yet, so you'll have to create a record
     # Hint: you won't want to create an artist record every time this method is called, only if an Drake is *not found*
-      
-      if !!(@drake != Artist.create(name: "Drake"))
-         @drake = Artist.create(name: "Drake")
-         self.artist = @drake
-      else
+      @drake = Artist.create(name: "Drake")
+      if !!(@drake = Artist.create(name: "Drake"))
          self.artist = @drake
       end
-      binding.pry 
+      # binding.pry 
   end
 end
+
+
+# if !!(@drake != Artist.create(name: "Drake"))
+#   @drake = Artist.create(name: "Drake")
+#   self.artist = @drake
+# else
+#   self.artist = @drake
+# end
+# binding.pry 
